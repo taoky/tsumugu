43d93fab4524d66c4bd27c5e58d32dcb4598c1b6
taoky <me@taoky.moe>
2024-08-26 00:24:25 +0800

    build: Fix version not showing for cargo install versions

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,15 @@ use crate::regex_process::ExpandedRegex;
 fn get_version() -> &'static str {
     let tag = build::TAG;
     let clean = build::GIT_CLEAN;
+    let short_commit = build::SHORT_COMMIT;
     if !clean {
         return Box::leak(format!("{} (dirty)", build::SHORT_COMMIT).into_boxed_str());
     } else if tag.is_empty() {
-        return build::SHORT_COMMIT;
+        if short_commit.is_empty() {
+            return build::PKG_VERSION;
+        } else {
+            return short_commit;
+        }
     } else {
         return tag;
     }
