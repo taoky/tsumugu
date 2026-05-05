f6bba78ef3b136510f4ea23a6b314c4e2b67ad8a
taoky <me@taoky.moe>
2026-05-05 13:52:12 +0800

    Always jump tsumugu for cargo-jump

--- release.sh
+++ release.sh
@@ -18,7 +18,7 @@ prev_tag=$(git describe --tags --abbrev=0)
 prev_version=$(get_full_version "$prev_tag")
 echo "Releasing version: $full_version (previous: $prev_version)"
 # cargo install cargo-jump; echo "See https://github.com/taoky/cargo-jump"
-cargo jump --old-tag="$prev_tag" "$full_version"
+cargo jump --always-jump tsumugu --old-tag="$prev_tag" "$full_version"
 ./scripts/check-release-version.sh "refs/tags/$version"
 git commit -a -m "Bump version to $full_version"
 git tag "$version" -m "$msg"
